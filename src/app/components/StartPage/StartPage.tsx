f64cfaf36286d5d1558010d3dd29d156fb79bb79
Muhamed <mutaliev96@outlook.com>
2021-12-21 12:33:21 +0100

    startPage improved design

--- src/app/components/StartPage/StartPage.tsx
+++ src/app/components/StartPage/StartPage.tsx
@@ -4,13 +4,55 @@ import './StartPage.css';
 const StartPage = () => {
   return (
     <div className="container">
-      <nav className="navStart">Navbar</nav>
-      <main>Main</main>
-      <div id="frühstück">Frühstück</div>
-      <div id="mittagessen">Mittagessen</div>
-      <div id="abendessen">Abendessen</div>
-      <div id="snacks">Snacks</div>
-      <div id="training">Training</div>
+      <nav className="navStart">Heute</nav>
+      <main>
+        <h1 className="h1Meal">Verbleibende Kalorien</h1>
+        <ul className="rechnung">
+          <li>1600</li>
+          <li>-</li>
+          <li>0</li>
+          <li>+</li>
+          <li>0</li>
+          <li>=</li>
+          <li>1600</li>
+        </ul>
+        <ul className="h2Main">
+          <li>Ziel</li>
+          <li></li>
+          <li>Nahrung</li>
+          <li></li>
+          <li>Training</li>
+          <li></li>
+          <li>Verbleib</li>
+        </ul>
+      </main>
+
+      <div id="frühstück">
+        <h1 className="h1Meal">Frühstück Kalorien:</h1>
+        <div className="addedFood">Paprika cal</div>
+        <h2 className="nahrungsmittelHinzu">Nahrungsmittel hinzufügen</h2>
+      </div>
+
+      <div id="mittagessen">
+        <h1 className="h1Meal">Mittagessen Kalorien:</h1>
+        <div className="addedFood"></div>
+        <h2 className="nahrungsmittelHinzu">Nahrungsmittel hinzufügen</h2>
+      </div>
+      <div id="abendessen">
+        <h1 className="h1Meal">Abendessen Kalorien:</h1>
+        <div className="addedFood"></div>
+        <h2 className="nahrungsmittelHinzu">Nahrungsmittel hinzufügen</h2>
+      </div>
+      <div id="snacks">
+        <h1 className="h1Meal">Snacks Kalorien:</h1>
+        <div className="addedFood"></div>
+        <h2 className="nahrungsmittelHinzu">Nahrungsmittel hinzufügen</h2>
+      </div>
+      <div id="training">
+        <h1 className="h1Meal">Training Kalorien:</h1>
+        <div className="addedFood"></div>
+        <h2 className="nahrungsmittelHinzu">Nahrungsmittel hinzufügen</h2>
+      </div>
     </div>
   );
 };
